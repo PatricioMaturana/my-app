9f969514d55da7c8b92b05c768c41c9044bc511b
Patricio Maturana <pmaturana@taglermaq-ad.cl>
2024-05-06 15:57:59 -0400

    Pre-Entrega2-Patricio Maturana

--- src/components/CartWidget .jsx
+++ src/components/CartWidget .jsx
@@ -2,7 +2,7 @@ const CartWidget = () => {
       return(
         <button type="button" className="btn btn-primary position-relative  bg-success">
             <i className="fa-solid fa-cart-shopping" ></i>
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">onAddCart</span>
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
         </button>
     )
 }
